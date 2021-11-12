1bfe75e85d835716ccf85bc4f617a1eae822d284
Yigitceli <yigit.celi@outlook.com>
2021-11-12 12:16:23 +0300

    Login setup

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var jwt = require("jsonwebtoken");
+const User = require("../db/Models/User");
+require("dotenv").config();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/login", async function (req, res, next) {
+  try {
+    let { username, passwordBody } = req.body;
+
+    (!username || !passwordBody) &&
+      res.status(402).json({
+        message: "Invalid input.",
+        succes: false,
+      });
+
+    const user = await User.query()
+      .select()
+      .where("username", username)
+      .first();
+
+    !user &&
+      res.status(404).json({
+        message: "There is no user with this username.",
+        succes: false,
+      });
+
+    user.password !== passwordBody &&
+      res.status(401).json({
+        message: "Wrong Password.",
+        succes: false,
+      });
+    const { password, ...info } = user;
+    const token = jwt.sign(info, process.env.SECRET, { expiresIn: "3d" });
+    res.cookie("token", token, { maxAge: 900000, httpOnly: true });
+    res.json({ ...info, token });
+  } catch (error) {}
 });
 
 module.exports = router;
